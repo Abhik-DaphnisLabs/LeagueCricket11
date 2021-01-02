57e291f4e9be0a2e90bc757c7bc7d44c42b5a454
Abhik Mehta <abhikmehta@usthaan.in>
2021-01-03 01:25:48 +0530

    leagueCricket11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'League Cricket 11';
+  companyName = "League Cricket 11";
 }
